738ad23959f91ce288a25ed7e3df3279db4d41bf
PeckZeg <peck.zeg@gmail.com>
2017-12-08 16:08:09 +0800

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
+const pathToRegexp = require('path-to-regexp');
 const router = module.exports = require('koa-router')();
 
-router.get('/', async (ctx, next) => {
+router.get(pathToRegexp('(/[^\/]+)+'), async (ctx, next) => {
   await ctx.render('index');
 });
